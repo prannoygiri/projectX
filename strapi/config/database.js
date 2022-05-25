bd5072fbc881954860959c31fa27f83afb516512
Prannoy <prannoy.giri@bighit.fans>
2022-05-25 15:56:45 +0530

    fix strapi configurations

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,12 +4,21 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('STRAPI_DATABASE_HOST', '127.0.0.1'),
+        port: env.int('STRAPI_DATABASE_PORT', 3306),
+        database: env('STRAPI_DATABASE_NAME', 'bighit'),
+        username: env('STRAPI_DATABASE_USER', 'admin'),
+        password: env('STRAPI_DATABASE_PASSWORD', 'dbpass'),
+        ssl: env.bool('DATABASE_SSL', false),
+        charset : "utf8"
       },
       options: {
-        useNullAsDefault: true,
-      },
+        autoMigration: true,
+        charset : "utf8"
+        //authenticationDatabase: env('AUTHENTICATION_DATABASE'),
+        //ssl: env('DATABASE_SSL')
+      }
     },
   },
 });
