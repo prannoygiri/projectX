c87cc1acf3b1cea3bfd55eb76cdc8b1bd47d1eb8
Jatin K <jatin@bighit.fans>
2022-05-29 16:49:37 +0530

    health check added

--- api/src/api.js
+++ api/src/api.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send("HELLO WORLD!")
 })
 
-app.listen(8081, function () {
-  console.log("APP listening on port 8081")
+app.listen(3000, function () {
+  console.log("APP listening on port 3000")
 })
